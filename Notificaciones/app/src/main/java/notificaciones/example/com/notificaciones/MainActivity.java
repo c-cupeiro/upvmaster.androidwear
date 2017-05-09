7e1bed9987cb6e14338f93c945e5189dbade3401
Carlos Casa <carlos.cupeiro@grupocmc.es>
2017-05-09 21:39:07 +0200

    Ejercicio 1 del tema

---
+++ Notificaciones/app/src/main/java/notificaciones/example/com/notificaciones/MainActivity.java
@@ -0,0 +1,30 @@
+package notificaciones.example.com.notificaciones;
+
+import android.app.Notification;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button wearButton = (Button) findViewById(R.id.boton1);
+        wearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int notificacionId = 001;
+                Notification notificacion = new NotificationCompat.Builder(MainActivity.this)
+                        .setSmallIcon(R.mipmap.ic_launcher).setContentTitle("Título").setContentText("Notificación Android Wear")
+                        .build();
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
+                notificationManager.notify(notificacionId, notificacion);
+            }
+        });
+    }
+}
