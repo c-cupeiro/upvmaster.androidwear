7dfdfcaa2e369d9d35d05a85d3e1a34cc1a12638
Carlos Casa <carlos.cupeiro@grupocmc.es>
2017-05-21 11:05:31 +0200

    Acciones para notificaciones solo wear pagina 43

--- Notificaciones/app/src/main/java/notificaciones/example/com/notificaciones/MainActivity.java
+++ Notificaciones/app/src/main/java/notificaciones/example/com/notificaciones/MainActivity.java
@@ -11,6 +11,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -28,11 +31,21 @@ public class MainActivity extends AppCompatActivity {
                 Intent intencionLlamar = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:555123456"));
                 PendingIntent intencionPendientetLlamar = PendingIntent.getActivity(MainActivity.this, 0, intencionLlamar, 0);
 
+                // Creamos la acción
+                NotificationCompat.Action accion = new NotificationCompat.Action.Builder(R.mipmap.ic_action_call,
+                        "llamar Wear", intencionPendientetLlamar).build();
+
+                //Creamos una lista de acciones
+                List<NotificationCompat.Action> acciones = new ArrayList<NotificationCompat.Action>(); acciones.add(accion);
+                acciones.add(new NotificationCompat.Action(R.mipmap.ic_action_locate, "Ver mapa", intencionPendienteMapa));
+
                 int notificacionId = 001;
                 Notification notificacion = new NotificationCompat.Builder(MainActivity.this)
                         .setSmallIcon(R.mipmap.ic_launcher).setContentTitle("Título").setContentText("Notificación Android Wear")
                         .setContentIntent(intencionPendienteMapa)
                         .addAction(R.mipmap.ic_action_call, "llamar", intencionPendientetLlamar)
+                        //.extend(new NotificationCompat.WearableExtender().addAction( accion))
+                        .extend(new NotificationCompat.WearableExtender().addActions(acciones))
                         .build();
                 NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
                 notificationManager.notify(notificacionId, notificacion);
