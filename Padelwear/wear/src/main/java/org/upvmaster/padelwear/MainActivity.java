c581630ee6e4b0d0f6e522733badc2f844348926
Carlos Casa <carlos.cupeiro@grupocmc.es>
2017-05-27 12:44:04 +0200

    Control circular

--- Padelwear/wear/src/main/java/org/upvmaster/padelwear/MainActivity.java
+++ Padelwear/wear/src/main/java/org/upvmaster/padelwear/MainActivity.java
@@ -35,5 +35,7 @@ public class MainActivity extends Activity {
         lista.setAdapter(adaptador);
         lista.setCenterEdgeItems(true);
         lista.setLayoutManager(new MyChildLayoutManager(this));
+        lista.setCircularScrollingGestureEnabled(true);
+        lista.setScrollDegreesPerScreen(180); lista.setBezelWidth(0.5f);
     }
 }
